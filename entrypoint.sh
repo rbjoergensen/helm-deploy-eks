f95d1c5d65e027ee74423e3ced099009fb71817b
Rasmus Bo Jørgensen <rbj@sampension.dk>
2021-04-24 01:11:25 +0200

    Run actual command in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ aws configure set aws_access_key_id $2
 aws configure set aws_secret_access_key $3
 aws configure set default.region $4
 
-response=$(aws eks list-clusters)
+response=$(sh -c "$1")
 
 echo "::set-output name=response::$response"
